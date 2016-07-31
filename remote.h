a53fa0ef49bd7824d2b75b04254e1306a4685108
WiseLord <wiselord1983@gmail.com>
2016-07-31 16:14:55 +0300

    Sync remote control code with ampcontrol project

--- remote.h
+++ remote.h
@@ -4,57 +4,90 @@
 #include <inttypes.h>
 #include "pins.h"
 
-#define RC5_SHORT_MIN				622		// 889 us ± 30%
-#define RC5_SHORT_MAX				1156
-#define RC5_LONG_MIN				1245	// 1778 us  ± 30%
-#define RC5_LONG_MAX				2773
+// Time scale definitions and macroses
+#define RC_TIMER_DIV				8	// 1MHz / 125kHz of Timer 1 => delays in us
 
-#define RC5_STBT_MASK				0x3000
-#define RC5_TOGB_MASK				0x0800
-#define RC5_ADDR_MASK				0x07C0
-#define RC5_COMM_MASK				0x003F
+#define NEC_DEV_MIN					0.7
+#define NEC_DEV_MAX					1.3
+#define NEC_MIN(delay)				((uint16_t)(delay / RC_TIMER_DIV * NEC_DEV_MIN))
+#define NEC_MAX(delay)				((uint16_t)(delay / RC_TIMER_DIV * NEC_DEV_MAX))
+#define NEC_NEAR(value, delay)		(value > NEC_MIN(delay) && value < NEC_MAX(delay))
 
-typedef enum {
-	EVENT_RC5_SHORT_SPACE = 0,
-	EVENT_RC5_SHORT_PULSE = 2,
-	EVENT_RC5_LONG_SPACE  = 4,
-	EVENT_RC5_LONG_PULSE  = 6,
-} RC5Event;
+#define RC6_DEV_MIN					0.8
+#define RC6_DEV_MAX					1.2
+#define RC6_MIN(delay)				((uint16_t)(delay / RC_TIMER_DIV * RC6_DEV_MIN))
+#define RC6_MAX(delay)				((uint16_t)(delay / RC_TIMER_DIV * RC6_DEV_MAX))
+#define RC6_NEAR(value, delay)		(value > RC6_MIN(delay) && value < RC6_MAX(delay))
 
-typedef enum {
-	STATE_RC5_START1 = 0,
-	STATE_RC5_MID1   = 1,
-	STATE_RC5_MID0   = 2,
-	STATE_RC5_START0 = 3,
-} RC5State;
-
-#define NEC_PULSE_WIDTH_MIN			392		//  560 us ± 30%
-#define NEC_PULSE_WIDTH_MAX			728
-#define NEC_ZERO_WIDTH_MIN			392		//  560 us ± 30%
-#define NEC_ZERO_WIDTH_MAX			728
-#define NEC_ONE_WIDTH_MIN			1176	// 1680 us ± 30%
-#define NEC_ONE_WIDTH_MAX			2184
-#define NEC_START_PULSE_WIDTH_MIN	6300	// 9000 us ± 30%
-#define NEC_START_PULSE_WIDTH_MAX	11700
-#define NEC_START_PAUSE_WIDTH_MIN	3150	// 4500 us ± 30%
-#define NEC_START_PAUSE_WIDTH_MAX	5850
-#define NEC_REPEAT_WIDTH_MIN		1575	// 2250 us ± 30%
-#define NEC_REPEAT_WIDTH_MAX		2925
-
+// Remote control types
 enum {
 	IR_TYPE_RC5,
 	IR_TYPE_NEC,
+	IR_TYPE_RC6,
+	IR_TYPE_SAM,
 
 	IR_TYPE_NONE = 0x0F
 };
 
+// Structure for strore data received
 typedef struct {
 	uint8_t ready : 1;
 	uint8_t repeat : 1;
-	uint8_t type : 6;
+	uint8_t rsvd1 : 1;
+	uint8_t rsvd2 : 1;
+	uint8_t type : 4;
 	uint8_t address;
 	uint8_t command;
 } IRData;
+
+// RC5/RC6 definitions
+#define RC6_1T						444
+#define RC6_2T						889
+#define RC6_3T						1333
+#define RC6_4T						1778
+#define RC6_6T						2667
+
+#define RC5_STBT_MASK				0x2000
+#define RC5_FIBT_MASK				0x1000
+#define RC5_TOGB_MASK				0x0800
+#define RC5_ADDR_MASK				0x07C0
+#define RC5_COMM_MASK				0x003F
+
+#define RC6_ADDR_MASK				0xFFC0
+#define RC6_COMM_MASK				0x00FF
+
+typedef enum {
+	STATE_RC5_MID0 = 0,
+	STATE_RC5_MID1,
+	STATE_RC5_START0,
+	STATE_RC5_START1,
+} RC5State;
+
+// NEC/Samsung definitions
+#define NEC_INIT					9000
+#define SAM_INIT					4500
+#define NEC_START					4500
+#define NEC_REPEAT					2250
+#define NEC_ZERO					560
+#define NEC_ONE						1680
+#define NEC_PULSE					560
+
+typedef enum {
+	STATE_NEC_IDLE = 0,
+	STATE_NEC_INIT,
+	STATE_NEC_REPEAT,
+	STATE_NEC_RECEIVE,
+} NECState;
+
+typedef union {
+	uint32_t raw;
+	struct {
+		uint8_t laddr;
+		uint8_t haddr;
+		uint8_t cmd;
+		uint8_t ncmd;
+	};
+} NECCmd;
 
 void rcInit(void);
 
