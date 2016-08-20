f60387b117278e9430401cf915be1d2aa42e1cef
WiseLord <wiselord1983@gmail.com>
2016-08-20 20:51:42 +0300

    Sync I2C code with ampcontrol project

--- i2c.c
+++ i2c.c
@@ -4,18 +4,25 @@
 
 void I2CInit(void)
 {
-	TWBR = 8;
-	TWSR = (1<<TWPS0);	/* Prescaler = 4, SCL=<8000000>/(16+2*<8>*<4>)=100000Hz */
-	TWCR |= (1<<TWEN);	/* Enable TWI */
+	// SCL = F_CPU / (16 + 2 * TWBR * prescaler)
+	// SCL = 8000000 / (16 + 2 * 32 * 1)
+
+	TWBR = 32;
+	//TWSR = (0<<TWPS1) | (0<<TWPS0);				// Prescaler = 1
+
+	TWCR |= (1<<TWEN);								// Enable TWI
+
 	return;
 }
 
 void I2CStart(uint8_t addr)
 {
 	uint8_t i = 0;
-	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTA);	/* Start */
-	while (!(TWCR & (1<<TWINT))) {				/* Wait for TWINT */
-		if (i++ > 250)	/* Avoid endless loop */
+
+	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTA);		// Start
+
+	while(bit_is_clear(TWCR, TWINT)) {
+		if (i++ > 250)								// Avoid endless loop
 			return;
 	}
 
@@ -27,47 +34,47 @@ void I2CStart(uint8_t addr)
 void I2CStop(void)
 {
 	uint8_t i = 0;
-	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);	/* Stop */
-	while (TWCR & (1<<TWSTO)) {					/* Wait for TWSTO */
-		if (i++ > 250)	/* Avoid endless loop */
-			return;
+
+	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);		// Stop
+
+	while (bit_is_set(TWCR, TWSTO)) {				// Wait for TWSTO
+		if (i++ > 250)								// Avoid endless loop
+			break;
 	}
+
 	return;
 }
 
-uint8_t I2CWriteByte(uint8_t data)
+void I2CWriteByte(uint8_t data)
 {
 	uint8_t i = 0;
-	TWDR = data;
-	TWCR = (1<<TWEN) | (1<<TWINT);	/* Start data transfer */
 
-	while (!(TWCR & (1<<TWINT))) {	/* Wait for finish */
-		if (i++ > 250)	/* Avoid endless loop */
-			return 1;
+	TWDR = data;
+	TWCR = (1<<TWEN) | (1<<TWINT);					// Start data transfer
+
+	while (bit_is_clear(TWCR, TWINT)) {				// Wait for finish
+		if (i++ > 250)								// Avoid endless loop
+			break;
 	}
 
-	if (TWSR_STA != 0x18 && TWSR_STA != 0x28 && TWSR_STA != 0x40)
-		return 1;
-	return 0;
+	return;
 }
 
-uint8_t I2CReadByte(uint8_t *data, uint8_t ack)
+uint8_t I2CReadByte(uint8_t ack)
 {
 	uint8_t i = 0;
+
 	if (ack)
 		TWCR |= (1<<TWEA);
 	else
 		TWCR &= ~(1<<TWEA);
+
 	TWCR |= (1 << TWINT);
 
-	while (!(TWCR & (1<<TWINT))) {	/* Wait for finish */
-		if (i++ > 250)	/* Avoid endless loop */
-			return 1;
+	while (bit_is_clear(TWCR, TWINT)) {				// Wait for finish
+		if (i++ > 250)								// Avoid endless loop
+			break;
 	}
 
-	if (TWSR_STA != 0x58 && TWSR_STA != 0x50)
-		return 1;
-
-	*data = TWDR;
-	return 0;
+	return TWDR;
 }
